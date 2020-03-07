a349cded4dd1975a7f4867d3875e4fdec1dc85ce
Rednecked_Crake <almas.abdibayev.gr@dartmouth.edu>
2020-03-07 14:53:52 -0500

    cleaned Printing

--- src/Printing.hs
+++ src/Printing.hs
@@ -39,39 +39,6 @@ showExpr (Con f [e]) = showString f . showSpace . showExpr e
 showExpr (Con f [e1,e2]) | isOp f
  = showParen True (showExpr e1 . showSpace . showString f . showSpace . showExpr e2)
 showExpr (Deriv v e) = showString "d/d" . showString v . showSpace . showExpr e
--- showExpr (Con f es)
---  = showParen True
---     (showString f . showSpace . showSep " " (showExpr) es)
-
-
-
-
-
--- showExpr :: Int -> Expr -> ShowS
--- showExpr _ (Var v) = showString v
--- showExpr _ (Val v) = showString (show v)
--- showExpr _ (Con f []) = showString f
--- showExpr p (Con f [e1,e2]) | isOp f
---  = showParen (p>0) (showExpr 1 e1 . showSpace . showString f . showSpace . showExpr 1 e2)
--- showExpr p (Con f es)
---  = showParen (p>1)
---     (showString f . showSpace . showSep " " (showExpr 2) es)
-
--- showExpr :: Int -> Expr -> ShowS
--- showExpr _ (Compose []) = showString "id"
--- showExpr p (Compose [a]) = showAtom p a
--- showExpr p (Compose as)
---  = showParen (p>0)
---     (showSep " . " (showAtom 1) as)
-
--- showAtom :: Int -> Atom -> ShowS
--- showAtom _ (Var v) = showString v
--- showAtom _ (Con f []) = showString f
--- showAtom p (Con f [e1,e2]) | isOp f
---  = showParen (p>0) (showExpr 1 e1 . showSpace . showString f . showSpace . showExpr 1 e2)
--- showAtom p (Con f es)
---  = showParen (p>1)
---     (showString f . showSpace . showSep " " (showExpr 2) es)
 
 showSep :: String -> (a -> ShowS) -> [a] -> ShowS
 showSep sep f = compose . intersperse (showString sep) . map f
@@ -92,15 +59,6 @@ parseExpr str = case parse expr "" str of
     Right e -> e
 
 
--- parseE :: String -> IO ()
--- parseE str = case parse expr "" str of
---     Left er -> putStr (errorBundlePretty er)
---     Right e -> putStrLn (showExpr e "")
-
--- parseE :: String -> IO ()
--- parseE str = case parse expr "" str of
---     Left er -> putStr (errorBundlePretty er)
---     Right e -> putStrLn (showExpr 0 e "")
 
 
 showLaw :: Law -> ShowS
